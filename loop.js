21ebdf674765fd3c2b94c73db33a46706bd44e01
bijay3474 <bijay3474@gmail.com>
2024-06-23 09:28:03 +0545

    second commit

---
+++ loop.js
@@ -0,0 +1,69 @@
+// for(i=1;i<5;i++){
+//      console.log("hello world")
+// }
+
+// for(i=1;i<=10;i++){
+//     console.log("the number is",i);
+// }
+//  for(i=1;i<=100;i++){
+//     if(i%2==0)
+//         console.log("the number is even",i)
+//     else
+//         console.log("the number is odd",i);
+//  }
+
+// for(i=0;i<=45;i++){
+//     if(i%3==0 && i%5==0)
+//         console.log("Fizzbuzz",i)
+//     else if(i%5==0)
+//         console.log(" Buzz",i)
+//     else if(i%3==0)
+//         console.log("Fizz",i)
+// }
+// let sum=0
+// for(i=0;i<=10;i++){
+//      sum=sum+i
+// }
+// {
+//     console.log("sum of first 10 natural number is",sum)
+// }
+
+// let a=5
+// for(i=1;i<=10;i++)
+//     {
+//         console.log(`${a}*${i}=${a*i}`) 
+
+//     }
+
+// let i=10
+// while(i>0){
+//     console.log(i--)
+// }
+
+// let i=10
+// do{
+//     console.log(i--)
+// }while(i>0)
+
+// let sum=0,i=1
+// let input=prompt("Enter n")
+// while(i<=input){
+//     sum=sum+i;
+//     i++;
+// }
+// console.log(sum)
+
+// let i=1
+// let input=prompt("Enter a number for multiplication")
+// while(i<=10){
+//     console.log(`${input}*${i}=${input*i}`)
+//     i++
+// }
+let input
+while(input>100){
+     input=prompt("Ankit")
+      console.log(input)
+
+}
+
+
